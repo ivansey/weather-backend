b6177f266dd7bed37c23f6052eb83831ba3c8eaf
ivansey <van.secret2015@gmail.com>
2021-11-04 12:01:53 +0300

    Fix bugs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
 const axios = require("axios");
 
 const app = express();
@@ -12,21 +13,37 @@ app.use(bodyParser());
 app.use(cors());
 
 app.post("/get/one", (req, res) => {
+	axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + String(req.body.city) + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
+		res.send({
+			weather: d.data,
+		})
+	})
+})
+
+app.post("/get/period", (req, res) => {
 	axios.get("https://api.openweathermap.org/geo/1.0/direct?q=" + String(req.body.city) + "&limit=1&appid=" + process.env.OWM_TOKEN).then(dt => {
 		axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + dt.data[0].lat + "&lon=" + dt.data[0].lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
 			res.send({
-				weather: d.data,
+				weather: d.data.weather,
 			})
 		}).catch(err => console.error(err))
 	})
 })
 
 app.post("/locate/one", (req, res) => {
-	axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + req.body.lan + "&lon=" + req.body.lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
+	axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + req.body.lan + "&lon=" + req.body.lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
 		res.send({
 			weather: d.data,
 		})
 	})
+})
+
+app.post("/locate/period", (req, res) => {
+	axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + dt.data[0].lat + "&lon=" + dt.data[0].lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
+		res.send({
+			weather: d.data.weather,
+		})
+	}).catch(err => console.error(err))
 })
 
 app.post("/find", (req, res) => {
