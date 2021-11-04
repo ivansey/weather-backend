38aaae7492e55651e0ddcdb56e33e8bc2ec3a856
ivansey <van.secret2015@gmail.com>
2021-11-04 11:54:58 +0300

    Add OneCall API get methods

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const {IamAuthenticator} = require('ibm-watson/auth');
-
-const languageTranslator = new LanguageTranslatorV3({
-	version: '2018-05-01',
-	authenticator: new IamAuthenticator({
-		apikey: 'eFyM8udgScH0IO1xqEJHh5FKWiA-WLMRToSBponuQ0gt',
-	}),
-	serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/457435fe-9e4a-47ed-98ec-ef77fd2a9d6a',
-	disableSslVerification: true,
-});
-
-const translit = require("cyrillic-to-translit-js");
-
 const axios = require("axios");
 
 const app = express();
@@ -26,20 +11,18 @@ const PORT = process.env.PORT;
 app.use(bodyParser());
 app.use(cors());
 
-function translateCity(city) {
-
-}
-
 app.post("/get/one", (req, res) => {
-	axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + String(req.body.city) + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
-		res.send({
-			weather: d.data,
-		})
+	axios.get("https://api.openweathermap.org/geo/1.0/direct?q=" + String(req.body.city) + "&limit=1&appid=" + process.env.OWM_TOKEN).then(dt => {
+		axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + dt.data[0].lat + "&lon=" + dt.data[0].lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
+			res.send({
+				weather: d.data,
+			})
+		}).catch(err => console.error(err))
 	})
 })
 
 app.post("/locate/one", (req, res) => {
-	axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + req.body.lan + "&lon=" + req.body.lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
+	axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + req.body.lan + "&lon=" + req.body.lon + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((d) => {
 		res.send({
 			weather: d.data,
 		})
@@ -47,21 +30,9 @@ app.post("/locate/one", (req, res) => {
 })
 
 app.post("/find", (req, res) => {
-	// languageTranslator.translate({
-	// 	text: req.body.city,
-	// 	modelId: "ru-en",
-	// })
-	// 	.then(translationResult => {
-	// 		console.log(translationResult.result.translations[0].translation);
-			axios.get("https://api.openweathermap.org/data/2.5/find?q=" + encodeURIComponent(req.body.city) + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((dt) => {
-				res.send(dt.data);
-			})
-		// })
-		// .catch(err => {
-		// 	console.log('error:', err);
-		// });
-
-
+	axios.get("https://api.openweathermap.org/data/2.5/find?q=" + encodeURIComponent(req.body.city) + "&lang=ru&units=metric&appid=" + process.env.OWM_TOKEN).then((dt) => {
+		res.send(dt.data);
+	})
 })
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
